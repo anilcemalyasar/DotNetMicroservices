10e2767d49449892120e197e5463968f7e0f55dc
Anil Yasar <anlysr2002@gmail.com>
2024-03-25 12:19:12 +0300

    coupon service and mvc frontend integration through httpclient

---
+++ Mango.Web/Models/CreateCouponRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Web.Models
+{
+    public class CreateCouponRequest
+    {
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
